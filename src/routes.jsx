0f53f7dd93b5ca12c8c494fba266a0dd127e29a4
Digoassun <digoassun@gmail.com>
2022-08-30 22:11:53 -0300

    Refatoração do arquivo de rotas

---
+++ src/routes.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Routes as Switcher, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Add from "./pages/Add/Add";
+import Cardapio from "./pages/Cardarpio/Cardapio";
+import Edit from "./pages/Edit/Edit";
+import Home from "./pages/Home/Home";
+import { GlobalStyle } from "./styles/globalStyles";
+
+const Routes = () => {
+  return (
+    <>
+      <GlobalStyle/>
+      <BrowserRouter>
+        <Header/>
+        <Switcher>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/cardapio" element={<Cardapio/>}/>
+          <Route path="/editar" element={<Edit/>}/>
+          <Route path="/adicionar" element={<Add/>}/>
+        </Switcher>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default Routes
