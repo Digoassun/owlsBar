dbf55c9641cbe5391c515f51bb42a67e41369030
Digoassun <digoassun@gmail.com>
2022-08-31 17:05:24 -0300

    Modificações simples no estilo global e configuração básica do context

--- src/context/OwlsBarProvider.jsx
+++ src/context/OwlsBarProvider.jsx
@@ -1,9 +1,13 @@
 import React, { createContext, useState } from "react";
 
-export const OwlsBarProvider = createContext()
+export const OwlsBarContext = createContext()
 
 const OwlsBarProvider =({ children} ) => {
 
+    const context ={}
+    return(
+        <OwlsBarContext.Provider value={context}>{children}</OwlsBarContext.Provider>
+    )
 }
 
 export default OwlsBarProvider
