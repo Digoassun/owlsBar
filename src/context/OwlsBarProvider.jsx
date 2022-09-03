9128869cd22a5e3a20e74a84e6d959c27582c551
Digoassun <digoassun@gmail.com>
2022-09-03 17:54:17 -0300

    Estado de login alterando Navbar

--- src/context/OwlsBarProvider.jsx
+++ src/context/OwlsBarProvider.jsx
@@ -3,24 +3,11 @@ import React, { createContext, useState } from "react";
 export const OwlsBarContext = createContext()
 
 const OwlsBarProvider =({children}) => {
-    const [login, setLogin] = useState(false)
-
-    const handleLoginTrue = ()=>{
-        setLogin(true)
-        console.log(login)
-
-    }
-    const handleLoginFalse = ()=>{
-        setLogin(false)
-        console.log(login)
-
-    }
+    const [login, setLogin] = useState(false)   
 
     const context ={
         login: login,
         setLogin: setLogin,
-        handleLoginTrue: handleLoginTrue,
-        handleLoginFalse: handleLoginFalse
     }
     return(
         <OwlsBarContext.Provider value={context}>{children}</OwlsBarContext.Provider>
